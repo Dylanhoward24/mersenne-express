711aa8f4205e99798342dab5fbf095f1b3152a22
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-29 11:31:17 -0500

    finished with some more lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,50 @@ function onReady() {
 
     // on page load, grab quote data from the server
     getQuotes();
+
+    // jQuery the button
+    console.log('submitBtn', $('#submitBtn'));
+    $('#submitBtn').on('click', addQuote);
+}
+
+function addQuote() {
+    console.log('inside addQuote');
+
+    // grab our quote data from the DOM inputs
+    let newQuote = {
+        text: $('#quoteInput').val(),
+        author: $('#authorInput').val()
+    }
+    console.log('newQuote is', newQuote);
+    
+    // POST our quote to /quotes
+    $.ajax({
+        method: 'POST',
+        url: '/quotes',
+        //send our quote data (object) to the server
+        data: newQuote,
+    }).then((response) => {
+        console.log('POST /quotes', response);
+
+        // refresh data from the server
+        // by calling the GET /quotes endpoint
+        // and re-rendering the DOM
+        //
+        // make sure to do this AFTER the POST request is complete
+        // (in our .then())
+        getQuotes();
+    }).catch((error) => {
+        console.log('POST /quotes failed!', error);
+        // render the error to the DOM
+        $('body').append(`
+            <h2>
+                Failed to save quote! 
+                Check your data, before you wreck your data
+            </h2>
+        `);
+        // or if we're feeling lazy, use a quick alert
+        // alert('Uh oh... something bad happened');
+    });
 }
 
 function getQuotes() {
@@ -33,6 +77,7 @@ function getQuotes() {
         console.log('quotes list element', quoteList);
 
         // loop through the array of quotes
+        quoteList.empty();
         for (let quote of response) {
             // render a <li> with each quote
             quoteList.append(`
